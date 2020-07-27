ca9e577209559d15ccdd8c55de6efc47cfb46973
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-27 15:51:38 +0500

    User JWT Auth Coded

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,8 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Tymon\JWTAuth\PayloadFactory;
 use Tymon\JWTAuth\JWTManager as JWT;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class UserController extends Controller
 {
@@ -35,5 +37,39 @@ class UserController extends Controller
         $token = JWTAuth::fromUser($user);
 
         return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function login(Request $request){
+        $credentials = $request->json()->all();
+
+        try{
+            if(! $token = JWTAuth::attempt($credentials)){
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        }
+        catch(JWTException $e){
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response()->json(compact('token'));
+    }
+
+    public function getAuthenticatedUser(){
+        try{
+            if(! $user = JWTAuth::parseToken()->authenticate()){
+                return response()->json(['user_not_found'], 404);
+            }
+        }
+        catch(TokenExpiredException $e){
+            return response()->json(['token_expired'], $e->getStatusCode());
+        }
+        catch(TokenInvalidException $e){
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        }
+        catch(JWTException $e){
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+
+        return response()->json(compact('user'));
     }
 }
