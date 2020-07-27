1ea8f013f801f492e997afe08df6848b3c4f232b
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-27 15:36:48 +0500

    User Controller Added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// JWT
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\PayloadFactory;
+use Tymon\JWTAuth\JWTManager as JWT;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $validator = Validator::make($request->json()->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->json()->get('name'),
+            'email' => $request->json()->get('email'),
+            'password' => Hash::make($request->json()->get('password')),
+        ]);
+
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+}
