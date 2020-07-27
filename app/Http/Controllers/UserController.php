a4396a0eb6700d97d2475efbba71480697a36c7c
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-27 16:25:52 +0500

    Error Solved now data is storing in a dB

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 // JWT
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -11,8 +15,9 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Tymon\JWTAuth\PayloadFactory;
 use Tymon\JWTAuth\JWTManager as JWT;
-use Tymon\JWTAuth\Exceptions\TokenExpiredException;
-use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+// use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+// use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class UserController extends Controller
 {
@@ -60,10 +65,10 @@ class UserController extends Controller
                 return response()->json(['user_not_found'], 404);
             }
         }
-        catch(TokenExpiredException $e){
+        catch(Tymon\JWTAuth\Exceptions\TokenExpiredException $e){
             return response()->json(['token_expired'], $e->getStatusCode());
         }
-        catch(TokenInvalidException $e){
+        catch(Tymon\JWTAuth\Exceptions\TokenInvalidException $e){
             return response()->json(['token_invalid'], $e->getStatusCode());
         }
         catch(JWTException $e){
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
+Route::get('profile', 'UserController@getAuthenticatedUser');
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
