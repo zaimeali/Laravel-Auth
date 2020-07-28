495bfa2463843da632550cbf49107c688ec06d87
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-28 12:39:38 +0500

    Profile UI Done

---
+++ resources/js/components/component/Navbar.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+import { Link, withRouter } from 'react-router-dom'
+
+class Navbar extends Component {
+
+    constructor(){
+        super()
+
+        this.logOut = this.logOut.bind(this)
+    }
+
+    logOut(e){
+        e.preventDefault();
+        localStorage.removeItem('usertoken');
+        this.props.history.push('/');
+    }
+
+    render() {
+        const loginRegLink = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                     <Link to="/login" className="nav-link">
+                        Login
+                     </Link>
+                </li>
+                <li className="nav-item">
+                     <Link to="/register" className="nav-link">
+                        Register
+                     </Link>
+                </li>
+            </ul>
+        )
+        const userLink = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                     <Link to="/profile" className="nav-link">
+                        Profile
+                     </Link>
+                </li>
+                <li className="nav-item">
+                     <a href="/" onClick={ this.logOut } className="nav-link">
+                        Logout
+                     </a>
+                </li>
+            </ul>
+        )
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+                <button 
+                    type="button" className="navbar-toggle"
+                    data-toggle="collapse"
+                    data-target="#navbar1"
+                    aria-controls="navbar1"
+                    aria-expanded="false"
+                    aria-label="Toggle Navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-md-center" id="navbar1">
+                    <ul className="navbar-nav">
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+                    </ul>
+                    { localStorage.usertoken ? userLink : loginRegLink }
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default withRouter(Navbar);
